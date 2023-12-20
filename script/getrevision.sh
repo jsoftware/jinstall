78a8f108dc046739c1e97c5737157163d414e886
Chris Burke <cdburke@gmail.com>
2023-12-20 09:15:12 -0800

    fixes to build

--- script/getrevision.sh
+++ script/getrevision.sh
@@ -2,6 +2,7 @@
 #
 # write revision.txt as full revision name, e.g. 9.5.0-beta3
 
+rm -f version*
 wget https://www.jsoftware.com/download/jengine/$1/version.txt
 R=`sed -n -e '/#define/p' version.txt | cut -d ' ' -f 3`
 R=${R//'"'}
