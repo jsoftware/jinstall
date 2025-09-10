e4a97ab2c5ea0fd651daebddc22afb397f2c5923
Chris Burke <cdburke@gmail.com>
2025-09-09 18:58:04 -0700

    test unixaio

--- script/build_unix_aio.sh
+++ script/build_unix_aio.sh
@@ -8,10 +8,6 @@ read -r V < release.txt
 V=j$V
 echo $V
 
-#script/getrevision.sh $V
-#read -r R < revision.txt
-#R=j$R
-
 D=https://www.jsoftware.com/download/$V
 
 T=/tmp/aio
@@ -24,77 +20,63 @@ rm -rf $W/*
 
 buildlist=""
 
-# mac full -------------------------------------------------------------
-cd $T
-F=${V}_mac64.zip
-I=jqt-mac.zip
-L=qt68-mac.zip
-wget $D/install/$F
-wget $D/qtide/$I
-wget $D/qtlib/$L
-unzip $F
-unzip $L
-cd $V
-mv ../Qt .
-cd bin
-unzip  ../../$I
-cd ../..
-id="$W/mac64_AIO.zip"
-buildlist+="${id},"
-echo $id >> buildlist.txt
-zip -9ry $id $V
+# mac ------------------------------------------------------------------
+f() {
+ if [[ "slim" = "$1" ]]; then
+   s="-slim";
+   t="_slim";
+ fi
+ rm -rf $T/$V/*
+ cd $T
+ F=${V}_mac64.zip
+ I=jqt-mac${s}.zip
+ L=qt68-mac${s}.zip
+ wget $D/install/$F
+ wget $D/qtide/$I
+ wget $D/qtlib/$L
+ unzip $F
+ unzip $L
+ cd $V
+ mv ../Qt .
+ cd bin
+ unzip  ../../$I
+ cd ../..
+ id="$W/mac64_AIO${t}.zip"
+ buildlist+="${id},"
+ echo $id >> buildlist.txt
+ zip -9ry $id $V
+}
 
-# mac slim -------------------------------------------------------------
-I=jqt-mac-slim.zip
-L=qt68-mac-slim.zip
-wget $D/qtide/$I
-wget $D/qtlib/$L
-unzip $L
-cd $V
-rm -rf Qt
-mv ../Qt .
-cd bin
-unzip -xvf ../../$I
-cd ../..
-id="$W/mac64_AIO_slim.zip"
-buildlist+="${id},"
-zip -9ry $id $V
+f ""
+f "slim"
 
-# linux full -----------------------------------------------------------
+# linux ----------------------------------------------------------------
+f () {
+ if [[ "slim" = "$1" ]]; then
+   s="-slim";
+   t="_slim";
+ fi
 rm -rf $T/$V/*
-cd $T
-F=${V}_linux64.tar.gz
-I=jqt-linux.tar.gz
-L=qt68-linux.tar.gz
+I=jqt-linux${s}.tar.gz
+L=qt68-linux${s}.tar.gz
 wget $D/install/$F
 wget $D/qtide/$I
 wget $D/qtlib/$L
 tar -xvf $F
 tar -xvf $L
 cd $V
-mv ../Qt .
-cd bin
-tar -xvf ../../$I
-cd ../..
-id="$W/linux64_AIO.tar.gz"
-buildlist+="${id},"
-tar -cvf - $V | gzip -9 - > $id
-
-# linux slim -----------------------------------------------------------
-I=jqt-linux-slim.tar.gz
-L=qt68-linux-slim.tar.gz
-wget $D/qtide/$I
-wget $D/qtlib/$L
-tar -xvf $L
-cd $V
 rm -rf Qt
 mv ../Qt .
 cd bin
 tar -xvf ../../$I
 cd ../..
-id="$W/linux64_AIO_slim.tar.gz"
+id="$W/linux64_AIO${t}.tar.gz"
 buildlist+="${id}"
 tar -cvf - $V | gzip -9 - > $id
+}
+
+f ""
+f "slim"
 
 # finish ---------------------------------------------------------------
 cd $S
